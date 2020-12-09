8b7b01f17c757a9c22f3398a9d4aa21563c01abf
Pierce Rogg <roggpierce@gmail.com>
2020-12-09 00:10:48 -0600

    added SQL commands to seed department and role tables

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES
+('IT'),
+('Human Resources'),
+('Engineering'),
+('Client Relations');
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+('Technical Support Rep', 35000, 1),
+('Technical Support Manager', 60000, 1),
+('HR Assistant', 25000, 2),
+('HR Manager', 40000, 2),
+('Junior Software Engineer', 45000, 3),
+('Senior Software Engineer', 75000, 3),
+('Customer Care Rep', 21000, 4),
+('Team Lead', 29000, 4);
